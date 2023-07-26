38c74f5b61c5cb208bbca97e87366111a1133bd3
NickooMar <nicoo.marsili@gmail.com>
2023-07-25 21:23:58 -0300

    Displaying notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-function HomePage() {
-  return (
-    <div>page</div>
-  )
+async function loadNotes() {
+  const baseURL = "http://localhost:3000/api/notes";
+  const res = await fetch(`${baseURL}`);
+  const notes = await res.json();
+  return notes;
 }
 
-export default HomePage
+async function HomePage() {
+  const notes = await loadNotes();
+
+  return (
+    <div>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h1>{note.title}</h1>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default HomePage;
